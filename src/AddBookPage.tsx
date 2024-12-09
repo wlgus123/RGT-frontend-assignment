f98ccc71242074d611d1385799935f08a897d0a4
wlgus123 <mail30019794@gmail.com>
2024-12-09 09:48:48 +0900

    chore(FE): 책 정보 추가 코드 다른 파일로 이동
    - AddBookPage.tsx파일로 이동
    - react-router-dom을 사용하여 주소 입력 시 그에 맞는 페이지로 이동
    
    Ref: #1

---
+++ src/AddBookPage.tsx
@@ -0,0 +1,87 @@
+import React, {useState} from "react";
+import {Book} from "./type.ts";
+
+const AddBookPage: React.FC = () => {
+  const [bookList, setBookList] = useState<Book[]>([]);
+  const [inputData, setInputData] = useState({
+    name: "",
+    author: "",
+    price: "",
+    introduce: "",
+  })
+
+  // inputData 입력 메서드
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData({
+      ...inputData,
+      name: e.target.value,
+    })
+  }
+  const handleAuthorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData({
+      ...inputData,
+      author: e.target.value,
+    })
+  }
+  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData({
+      ...inputData,
+      price: e.target.value,
+    })
+  }
+  const handleIntroduceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputData({
+      ...inputData,
+      introduce: e.target.value,
+    })
+  }
+
+  const handleAddBookInfo = () => {
+    // bookList에 값 넣기
+    setBookList([
+      ...bookList,
+      {
+        id: bookList.length + 1,
+        name: inputData.name,
+        author: inputData.author,
+        price: Number(inputData.price),
+        introduce: inputData.introduce
+      }
+    ])
+
+    // inputData 초기화
+    setInputData({
+      name: "",
+      author: "",
+      price: "",
+      introduce: "",
+    })
+  }
+
+  return (
+    <div>
+      {/* 책 정보 추가 */}
+      <div className="add-book-info">
+        <input type="text" name="name" placeholder="도서명 입력" value={inputData.name} onChange={handleNameChange}/>
+        <input type="text" name="author" placeholder="저자 입력" value={inputData.author} onChange={handleAuthorChange}/>
+        <input type="number" name="price" placeholder="가격 입력" value={inputData.price} onChange={handlePriceChange}/>
+        <input type="text" name="introduce" placeholder="책 소개 입력" value={inputData.introduce} onChange={handleIntroduceChange}/>
+        <button onClick={handleAddBookInfo}>추가하기</button>
+      </div>
+
+      {/* 책 리스트 */}
+      <div>
+        <ul>
+          {bookList.map((book) => (
+            <li key={book.id}>
+              <strong>{book.name}</strong> - {book.author} ({book.price}원) <br/>
+              소개: {book.introduce}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default AddBookPage
